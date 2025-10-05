de4a292b0d0bb77f7998d5e0e5430f7d421cf7fd
Luca Vitucci <luca@vexdev.com>
2025-10-05 15:13:00 +0200

    kernel update WiP

--- package/uvc-gadget/uvc-gadget.mk
+++ package/uvc-gadget/uvc-gadget.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UVC_GADGET_VERSION = e1c738181746876a9b9ca75fd12b9d565ae75644
+UVC_GADGET_VERSION = 04c18aa6c4a7017957e2dfe88c3ff7ef34a1b3a0
 UVC_GADGET_SITE = https://gitlab.freedesktop.org/camera/uvc-gadget.git
 UVC_GADGET_LICENSE = GPL-2.0+
 UVC_GADGET_LICENSE_FILES = LICENSE
